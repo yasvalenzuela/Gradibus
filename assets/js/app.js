b695f4d8b6c092e1cea64d1247f8816e57ed81cf
yasvalenzuela <yasna.valenzuela91@gmail.com>
2018-01-03 12:29:43 -0300

    subiendo cambios de js y css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,63 @@
+var registrar = document.getElementById('btn');
+registrar.addEventListener('click', function(){
+  /*console.log('diste un click');*/
+  var nombre = document.getElementById('nombre').value;
+  var correo = document.getElementById('correo').value;
+  var contraseña = document.getElementById('contraseña').value;
+
+  console.log(nombre);
+  console.log(correo);
+  console.log(contraseña);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 $(document).ready(function(){
   /*
   *formulario desplegable comience oculto
   */
+
+/*
   $('#containerIcono').hide();
   $('#formularioOculto').hide();
-  /*
+
   *evento de click al icono
   *que esta al lado de iniciar sesion
   */
+/*
 	$('#iconoUno').click(function(){
     $('#containerIcono').show();
     $('#formularioOculto').show();
@@ -16,7 +66,22 @@ $(document).ready(function(){
   *desahibilitar el boton
   *del formulario desplegable oculto
   */
-  $('#btnOculto').attr('disabled', true);
-
-
+  /*$('#btnOculto').attr('disabled', true);*/
+  /*
+  $('#btnOculto').click(function(){
+    $('#primerHeader').hide();
+    $('#fondo').hide();
+  })
 });
+*/
+
+
+/*
+*var correo = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+*if($('#correo').val()===""){
+*$('#correo').focus().after("<span class='error'>Ingrese correo electrónico correcto</span>");
+*}
+*
+*
+*
+*/
