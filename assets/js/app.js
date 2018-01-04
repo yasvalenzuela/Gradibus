43ce2a28dc4ace3d0f824ed39fd9f77cead0e776
Paula83 <andreapaulagm@gmail.com>
2018-01-04 17:03:42 -0300

    subiendo parte paula

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,12 +20,41 @@ registrar.addEventListener('click', function(){
   console.log(contrasena);
 })
 
-/*sección slider*/
-
-
-
-
-
+/*sección Post*/
+  $('.btn').click(function(){
+        var post = $('#postea').value();
+        $('#postea').value('');
+      
+      });
+    
+  $('#myCarousel').carousel({
+      interval: 5000
+          });
+   
+          //Handles the carousel thumbnails
+          jQuery(document).ready(function($) {
+   
+          $('#myCarousel').carousel({
+                  interval: 5000
+          });
+   
+          //Handles the carousel thumbnails
+          $('[id^=carousel-selector-]').click(function () {
+          var id_selector = $(this).attr("id");
+          try {
+              var id = /-(\d+)$/.exec(id_selector)[1];
+              console.log(id_selector, id);
+              jQuery('#myCarousel').carousel(parseInt(id));
+          } catch (e) {
+              console.log('Regex failed!', e);
+          }
+      });
+          // When the carousel slides, auto update the text
+          $('#myCarousel').on('slid.bs.carousel', function (e) {
+                   var id = $('.item.active').data('slide-number');
+                  $('#carousel-text').html($('#slide-content-'+id).html());
+          });
+  });
 
 
 
